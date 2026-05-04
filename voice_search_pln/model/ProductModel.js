1acb49fb8adb43f8d7fb649a6f7b7e7df5a314b1
Matheus Garibaldi <matheus.garibaldi@gft.com>
2026-05-04 15:11:45 -0300

    added more products on d

--- voice_search_pln/model/ProductModel.js
+++ voice_search_pln/model/ProductModel.js
@@ -1,11 +1,17 @@
 const products = [
     { id: "1", name: "notebook dell" },
-    { id: "2", name: "notebook apple" }
+    { id: "2", name: "notebook apple" },
+    { id: "3", name: "notebook lenovo" },
+    { id: "4", name: "notebook acer" },
+    { id: "5", name: "notebook samsung" },
+    { id: "6", name: "mouse logitech" },
+    { id: "7", name: "teclado mecânico redragon" },
+    { id: "8", name: "monitor lg 24 polegadas" },
+    { id: "9", name: "headset hyperx cloud" },
+    { id: "10", name: "cadeira gamer dxracer" }
 ]
 
 function search(term) {
-
-
     return products.find(p => p.name.includes(term))
 }
 
